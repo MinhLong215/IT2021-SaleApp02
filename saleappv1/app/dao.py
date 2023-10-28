a8106b79508db8c70a5109da2827f58f1d2e1a61
MinhLong215 <2151010201long@ou.edu.vn>
2023-10-28 16:16:28 +0700

    init project

---
+++ saleappv1/app/dao.py
@@ -0,0 +1,43 @@
+
+
+def load_categories():
+    return [{
+        'id' :  1,
+        'name' : 'Mobile'
+    }, {
+        'id' :  1,
+        'name' : 'Tablet'
+    }]
+
+def load_products(kw=None):
+    products = [{
+        "id": 1,
+        "name": "Iphone 15 Pro Max",
+        "price": 30000000,
+        "image": "https://cdn.hoanghamobile.com/i/preview/Uploads/2023/09/13/iphone-15-pro-natural-titanium-pure-back-iphone-15-pro-natural-titanium-pure-front-2up-screen-usen.png"
+    },{
+        "id": 2,
+        "name": "Iphone 15 Pro Max",
+        "price": 30000000,
+        "image": "https://cdn.hoanghamobile.com/i/preview/Uploads/2023/09/13/iphone-15-pro-natural-titanium-pure-back-iphone-15-pro-natural-titanium-pure-front-2up-screen-usen.png"
+    },{
+        "id": 1,
+        "name": "Iphone 15 Pro Max",
+        "price": 30000000,
+        "image": "https://cdn.hoanghamobile.com/i/preview/Uploads/2023/09/13/iphone-15-pro-natural-titanium-pure-back-iphone-15-pro-natural-titanium-pure-front-2up-screen-usen.png"
+    },{
+        "id": 1,
+        "name": "Ipad Pro Max",
+        "price": 30000000,
+        "image": "https://cdn.hoanghamobile.com/i/preview/Uploads/2023/09/13/iphone-15-pro-natural-titanium-pure-back-iphone-15-pro-natural-titanium-pure-front-2up-screen-usen.png"
+    },{
+        "id": 1,
+        "name": "Iphone 15 Pro Max",
+        "price": 30000000,
+        "image": "https://cdn.hoanghamobile.com/i/preview/Uploads/2023/09/13/iphone-15-pro-natural-titanium-pure-back-iphone-15-pro-natural-titanium-pure-front-2up-screen-usen.png"
+    }]
+
+    if kw:
+        products = [p for p in products if p['name'].find(kw) >= 0]
+
+    return products
